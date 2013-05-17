946b6495caca17ebc5946d1da5ee6c6c2611ebfd
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2013-05-17 18:20:28 -0500

    Terminamos Gauss-Seidel.

--- punto2/gausss.m
+++ punto2/gausss.m
@@ -1,30 +1,30 @@
 
-
-
-% Sistemas no lineales 2x2. Solución iterativa.
 clear all
-% Solucion de un sistema de orden dos diagonalmente dominante
-% x=g1(x,y); y=g2(x,y);
-n=10;
 
-g1='x^2-x+cos(y)-2*y^2+3';
-g2='sin(x+y)-2*x-4*y^2+9';
-% g3=input('g3(x,y)=');
-xi = 1.41421356237;
-yi = 1.73205080757;
+n=15;
 
+g1 = '-(3-cos(y)-z)^(1/2)';
+g2 = '((6-x-z)/2)^(1/2)';
+g3 = '(3-sin(x+y))/2';
 
-% Se genera una matriz M cuya primera fila es la semilla
+xi=3;
+yi=2;
+zi=1;
+
+%Se genera una matriz cuya primera fila es la semilla
 M(1,1)=xi;
 M(1,2)=yi;
+M(1,3)=zi;
 
 for k=1:n;
     x=M(k,1);
     y=M(k,2);
+    z=M(k,3);
     M(k+1,1)=eval(g1);
     x=M(k+1,1);
     M(k+1,2)=eval(g2);
+    y=M(k+1,2);
+    M(k+1,3)=eval(g3);
 end
 
 M
-
