0de2cc6094bc830d785c1028c57bf3e2401814ff
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2013-05-17 17:59:26 -0500

    Gaus.

--- punto2/gausss.m
+++ punto2/gausss.m
@@ -1,34 +1,30 @@
 
-clear all
-% Solucion de un sistema de orden tres diagonalmente dominante
-% x=g1(y,z); y=g2(x,z); z=g3(x,y)
-% n número de iteraciones
-% Todas las semillas se inicializan de acuerdo con la recomendación previa
 
-n=15;
+
+% Sistemas no lineales 2x2. Solución iterativa.
+clear all
+% Solucion de un sistema de orden dos diagonalmente dominante
+% x=g1(x,y); y=g2(x,y);
+n=10;
 
 g1='x^2-x+cos(y)-2*y^2+3';
 g2='sin(x+y)-2*x-4*y^2+9';
 % g3=input('g3(x,y)=');
-xi=-4;
-yi=4;
+xi = 1.41421356237;
+yi = 1.73205080757;
 
-% zi=input('zinicial=');
 
 % Se genera una matriz M cuya primera fila es la semilla
 M(1,1)=xi;
 M(1,2)=yi;
-% M(1,3)=zi;
 
 for k=1:n;
     x=M(k,1);
     y=M(k,2);
-    % z=M(k,3);
-    M(k+1,1) = eval(g1);
+    M(k+1,1)=eval(g1);
     x=M(k+1,1);
-    M(k+1,2) = eval(g2);
-    % y=M(k+1,2);
-    % M(k+1,3) = eval(g3);
+    M(k+1,2)=eval(g2);
 end
 
 M
+
