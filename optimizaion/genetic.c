12647d0fa5172d7c03cb9b7b686d5357b548a2e3
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2013-06-03 20:14:53 -0500

    Cambiamos float por double.
    Cambiamos float por double para mejorar la precisión.

--- optimizaion/genetic.c
+++ optimizaion/genetic.c
@@ -17,8 +17,8 @@ double drand48();
 
 typedef struct __pair__f__
 {
-    float n1;
-    float n2;
+    double n1;
+    double n2;
 } pair_f;
 
 typedef struct __pair__i__
@@ -27,12 +27,12 @@ typedef struct __pair__i__
     int n2;
 } pair_i;
 
-float function(float x, float y)
+double function(double x, double y)
 {
     return 20 + x*x + y*y - 10 * (cos(2 * M_PI * x) + cos(2 * M_PI * y));
 }
 
-int index_max(float * arr)
+int index_max(double * arr)
 {
     int i, i_max = 0;
 
@@ -55,23 +55,23 @@ double cross(double n1, double n2, int cross)
     return (((int)(n1*N_FLOAT) & ~a) | ((int)(n2*N_FLOAT) & a)) / N_FLOAT_D;
 }
 
-void new_population(float * popul_x, float * popul_y, float * prob)
+void new_population(double * popul_x, double * popul_y, double * prob)
 {
     int i, i_max = 0;
     int n_popul[4];
-    float n_popul_x[4];
-    float n_popul_y[4];
-    float prob_cpy[4];
+    double n_popul_x[4];
+    double n_popul_y[4];
+    double prob_cpy[4];
 
     // hago una copia del vector de probabilidad
-    memcpy(prob_cpy, prob, sizeof(float)*N);
+    memcpy(prob_cpy, prob, sizeof(double)*N);
 
     // encuentro la probabilidad mayor dentro del arreglo.
     n_popul[0] = n_popul[1] = i_max = index_max(prob_cpy);
-    prob_cpy[i_max] = -1.0f;
+    prob_cpy[i_max] = -1.0;
 
     n_popul[2] = index_max(prob_cpy);
-    prob_cpy[i_max] = -1.0f;
+    prob_cpy[i_max] = -1.0;
     n_popul[3] = index_max(prob_cpy);
 
     for(i = 0; i < N; i++)
@@ -80,7 +80,7 @@ void new_population(float * popul_x, float * popul_y, float * prob)
         n_popul_y[i] = popul_y[n_popul[i]];
     }
 
-    pair_i mating_order[4] = {0, 2, 1, 2, 2, 3, 1, 3};
+    pair_i mating_order[4] = {0, 2, 1, 2, 2, 3, 0, 3};
 
     int o_cross[4];
     o_cross[0] = N_CROSS - (rand() % N_CROSS);
@@ -96,19 +96,19 @@ void new_population(float * popul_x, float * popul_y, float * prob)
     }
 }
 
-float genetic(float (*f)(float, float), pair_f limit_x, pair_f limit_y, int n)
+double genetic(double (*f)(double, double), pair_f limit_x, pair_f limit_y, int n)
 {
     srand(time(NULL));
     srand48(time(NULL));
 
     register int i;
     int ii;
-    float w_x = limit_x.n2 - limit_x.n1;
-    float w_y = limit_y.n2 - limit_y.n1;
-    float popul_x[4];
-    float popul_y[4];
-    float p_total = 0.0f;
-    float p[4];
+    double w_x = limit_x.n2 - limit_x.n1;
+    double w_y = limit_y.n2 - limit_y.n1;
+    double popul_x[4];
+    double popul_y[4];
+    double p_total = 0.0;
+    double p[4];
 
     for(i = 0; i < N; i++)
     {
@@ -133,16 +133,16 @@ float genetic(float (*f)(float, float), pair_f limit_x, pair_f limit_y, int n)
 
         new_population(popul_x, popul_y, p);
 
-	puts("");
-	for(i = 0 ; i < N; i++)
-	    printf("popul %d: (%.4f, %.4f) = \t%.4f\n", ii, popul_x[i], popul_y[i], f(popul_x[i], popul_y[i]));
+        puts("");
+        for(i = 0 ; i < N; i++)
+            printf("popul %d: (%.4f, %.4f) = \t%.4f\n", ii, popul_x[i], popul_y[i], f(popul_x[i], popul_y[i]));
     }
 }
 
 int main()
 {
-    pair_f lx = {0.0f, 2.0f};
-    pair_f ly = {0.0f, 2.0f};
+    pair_f lx = {0.0, 2.0};
+    pair_f ly = {0.0, 2.0};
 
     genetic(function, lx, ly, 10);
 }
