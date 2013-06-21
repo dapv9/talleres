85d4fb87b82479266045fef0fbbfce7571163636
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2013-06-21 13:37:37 -0500

    Mejoramos gráfica 1.

--- metodos/entrega2/entrega2.m
+++ metodos/entrega2/entrega2.m
@@ -1,11 +1,38 @@
 
+clear all
+
 % Gráfica
+f = '1 + sin(x^2)';
 x = 0:0.01:2;
 y = func(x);
-% plot(x, y);
-% title('f(x) = 1 + sen(x^2); 0 <= x <= 2');
-% xlabel('x');
-% ylabel('y');
+
+
+% Gráfica de la función completa
+subplot(2, 1, 1);
+
+f_g = ezplot(f);
+set(f_g, 'color', 'r');
+
+title('f(x) = 1 + sen(x^2)');
+xlabel('x');
+ylabel('y');
+
+grid on
+
+
+% Gráfica de la función en la región de interes
+subplot(2, 1, 2);
+
+plot(x, y);
+
+title('f(x) = 1 + sen(x^2); 0 <= x <= 2');
+xlabel('x');
+ylabel('y');
+
+grid on
+
+
+
 
 % tabla de datos
 paso = 2.0/6.0;
@@ -21,7 +48,7 @@ disp(' ')
 disp([ [1:length(x)]' x' y'])
 
 % newton-cotes
-%  ncotes(x, y, paso)
+ncotes(x, y, paso)
 
 % interpolación cúbica
 
---
+++ metodos/entrega2/grafica1.m
@@ -0,0 +1,34 @@
+
+clear all
+
+% Gráfica
+f = '1 + sin(x.^2)';
+x = 0:0.01:2;
+y = eval(f);
+
+y
+
+
+% Gráfica de la función completa
+subplot(2, 1, 1);
+
+f_g = ezplot(f);
+set(f_g, 'color', 'r');
+
+title('f(x) = 1 + sen(x^2)');
+xlabel('x');
+ylabel('y');
+
+grid on
+
+
+% Gráfica de la función en la región de interes
+subplot(2, 1, 2);
+
+plot(x, y);
+
+title('f(x) = 1 + sen(x^2); 0 <= x <= 2');
+xlabel('x');
+ylabel('y');
+
+grid on
