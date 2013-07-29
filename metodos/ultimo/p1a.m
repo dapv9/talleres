e1e17185c0a2db0c26535af6eb250d2bb137c5ae
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2013-07-28 22:37:34 -0500

    Iniciamos la última tarea de numérico.

---
+++ metodos/ultimo/p1a.m
@@ -0,0 +1,24 @@
+
+
+clear all
+
+funcion = 'x*D2y+2*Dy-x*y=x'
+r1 = 'y(1)=0';
+r2 = 'y(3)=2';
+
+s = dsolve(funcion, r1, r2, 'x');
+
+pretty(s);
+
+syms x
+x = 0:0.01:2;
+y = eval(s);
+
+%  f_g = ezplot(s);
+plot(x, y)
+
+title(funcion);
+xlabel('x');
+ylabel('y');
+
+grid on
